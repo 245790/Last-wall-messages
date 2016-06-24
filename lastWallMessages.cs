9000421e8f6c0d53559217789ceaa6b2de41d4b8
Karim <karim_karim_1997@mail.ru>
2016-06-24 07:24:20 +0300

    Compiles, runs, incorrect result

--- lastWallMessages.cs
+++ lastWallMessages.cs
@@ -10,18 +10,22 @@ public class LastWallMessages
 {
     public static void Main(String[] args)
     {
-        String domain = "kaneua"; // short address of a user
-        int messageCount = 5; // amount of messages to read
+        String domain = "kaneua";                  // short address of a user
+        int messageCount = 5;                      // amount of messages to read
         String requestString = String.Format(
                     "https://api.vk.com/method/wall.get?domain={0}&count={1}",
                     domain,
                     messageCount);
         using (WebClient wc = new WebClient())
         {
+            // Now i'm going to just count all messages from a wall. Correct
+            // result should be 796.
             String json = wc.DownloadString(requestString);
-            var responseDefinition = new { response = new { count = 0 } };
+            var responseDefinition = new { count = 11 }; // 11 is used for type
+                                                         // inference
             var result = JsonConvert.DeserializeAnonymousType(json, responseDefinition);
-            Console.WriteLine(result.response.count);
+            Console.WriteLine(result.count);
         }
     }
 }
+// Prints 0
